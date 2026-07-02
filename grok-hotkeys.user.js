c610298b67cf897f24c101c7429be3f4b45e5021
eldmans <106721250+eldmans@users.noreply.github.com>
2026-07-02 05:28:13 +0300

    Update grok-hotkeys.user.js
    nothing done

--- grok-hotkeys.user.js
+++ grok-hotkeys.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Grok Hotkeys
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.200
 // @description  Keyboard shortcuts for Grok Imagine: PageDown = Download, PageUp = Upscale/Enhance, RightCtrl+Delete = Delete video, End = Toggle sound, Home = Saved history (current tab), Ctrl+Home = new tab. Works on both Russian and English UI.
 // @author       Grok + eldmans
 // @match        *://grok.com/*
